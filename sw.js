aead6631cda2160c0cd0c12b1c6cfa716dedb2ca
ilham030 <i_memmedli11@mail.ru>
2026-07-19 20:27:41 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'minibazar-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './config.js',
+  './script.js'
+];
+
+// Quraşdırma mərhələsi (Keşləmə)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Sayta daxil olanda məlumatları keşdən və ya internetdən yükləmək
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
